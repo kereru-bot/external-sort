724523003002db2636e7cc2713d06259569d65b0
joe <grombus@mypc.com>
2024-03-18 18:16:17 +1300

    bleh

--- xSort2.java
+++ xSort2.java
@@ -9,12 +9,60 @@ class xSort2 {
         int runSize = Integer.parseInt(args[0]);
         String filename = args[1];
         int mergesPerPass = Integer.parseInt(args[2]);
-
-
+        File[] left = new File[mergesPerPass];
+        File[] right = new File[mergesPerPass];
+        for(int i = 0; i < mergesPerPass; i++) {
+            left[i] = new File("l_" + i);
+            right[i] = new File("r_" + i);
+        }
+        int uniqueFiles = distributeRuns(filename, left, runSize);
         //create k files based on mergesperpass values
         //use distributeRuns on the files to distribute runs
         //across all of them
+        int writingSide = 1;
+        while(true) {
+            boolean done = false;
+            if((writingSide % 2) == 0) {
+                //reading side writing side
+                done = merge(right, left, runSize);
+                if(done) {
+                    //left one has everything
+                    try {
+                        BufferedReader reader = new BufferedReader(new FileReader(left[0]));
+                        String next;
+                        while((next = reader.readLine()) != null) {
+                            System.out.println(next);
+                        }
+                        reader.close();
+                    } catch (IOException ex) {
+                        System.err.println("FileNotFoundException: " + ex);
+                    }
+                } else {
 
+                }
+            } else {
+                System.out.println("merge correct");
+                done = merge(left, right, runSize);
+                if(done) {
+                    //right one has everthing
+                    try {
+                        BufferedReader reader = new BufferedReader(new FileReader(right[0]));
+                        String next;
+                        //System.out.println("done");
+                        while((next = reader.readLine()) != null) {
+                            System.out.println(next);
+                        }
+                        reader.close();
+                    } catch (IOException ex) {
+                        System.err.println("FileNotFoundException: " + ex);
+                    }
+                }
+            }
+
+            runSize*= uniqueFiles;
+            writingSide++;
+
+        }
         //after that, i will have up to k files with all of the runs inside them
     }
 
@@ -46,6 +94,8 @@ class xSort2 {
                     totalFilesWrittenTo++;
                     writer = new FileWriter(files[currentFile], true);
                     linesRead = 0;
+                } else {
+                    writer.write("\n");
                 }
 
             }
@@ -76,27 +126,14 @@ class xSort2 {
 
             String[] references = new String[readingSide.length];
             //ASSUME THAT ALL READINGSIDE FILES HAVE SOMETHING TO READ
-            String[] initLines = new String[readingSide.length];
             int[] linesRead = new int[readingSide.length];
 
-            minHeap heap = populateHeap(references, readers, linesRead);
-
-            for(int i = 0; i < readingSide.length; i++) {
-                readers[i] = new BufferedReader(new FileReader(readingSide[i]));
-                String next;
-                if((next = readers[i].readLine()) != null) {
-                    references[i] = next;
-                    initLines[i] = next;
-                    linesRead[i]++;
-                }
-            }
             //initialise heap
-            //heap = new minHeap();
-            //heap.createHeap(initLines, readingSide.length);
+            minHeap heap = populateHeap(readers,references,linesRead);
 
             //NOW HEAP IS SET UP AND READY FOR MERGE
             //ASSUME THAT WRITING SIDE FILES ARE EMPTY FOR EACH PASS
-            FileWriter writer;
+            FileWriter writer = null;
             int currentReadingFile = 0;
             int currentWritingFile = 0;
             String next;
@@ -106,6 +143,7 @@ class xSort2 {
 
             while(true) {
                 next = heap.pop();
+                System.out.println(next);
                 if(next == null) {
                     boolean finished = true;
                     //check for end of runs/end of stream
@@ -113,6 +151,13 @@ class xSort2 {
                         if(!b) {
                             finished = false;
                         } else {
+                            for(BufferedReader buf : readers) {
+                                buf.close();
+                            }
+
+                            if(writer != null) {
+                                writer.close();
+                            }
                             return singleFileWritten;
                             //return true if everything is done
                         }
@@ -132,6 +177,14 @@ class xSort2 {
                         } else {
                             currentWritingFile++;
                         }
+                        //reset linesread etc.
+                        for(int i = 0; i < linesRead.length; i++) {
+                            linesRead[i] = 0;
+                            references[i] = null;
+                            endOfRun[i] = false;
+                        }
+                        heap = populateHeap(readers,references,linesRead);
+                        next = heap.pop();
                     }
 
                 }
@@ -165,16 +218,10 @@ class xSort2 {
                         break;
                     }
                 }
-
-
             }
-            
-
-
-
-            
         } catch (FileNotFoundException ex) {
             System.err.println("FileNotFoundException: " + ex);
+            System.err.println("this one");
         } catch (IOException ex) {
             System.err.println("IOException: " + ex);
         }
@@ -185,8 +232,23 @@ class xSort2 {
     }
 
     public static minHeap populateHeap(BufferedReader[] readers, String[] references, int[] linesRead) {
+        try {
+            String[] heapLines = new String[readers.length];
+            for(int i = 0; i < readers.length; i++) {
+                String next;
+                if((next = readers[i].readLine()) != null) {
+                    references[i] = next;
+                    linesRead[i]++;
+                    heapLines[i] = next;
+                }
+            }
 
-
+            minHeap heap = new minHeap();
+            heap.createHeap(heapLines, heapLines.length);
+            return heap;
+        } catch (IOException ex) {
+            System.err.println("IOException ex: " + ex);
+        }
         return null;
     }
 }
